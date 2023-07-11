b9b6419148b5d384b44c56a1d21bb6c0c5ed6941
raheem6 <raheemabuhumra@csus.edu>
2023-07-11 12:27:22 -0700

    commit!

--- src/Main.java
+++ src/Main.java
@@ -8,8 +8,8 @@ public class Main {
         System.out.println("Age = " + tim.getAge());
         System.out.println("Pay = " + tim.collectPay());
 
-        Employee joe = new Employee("Joe", "11/11/1990",
-                "03/03/2020");
+        SalariedEmployee joe = new SalariedEmployee("Joe", "11/11/1990",
+                "03/03/2020", 2400);
         System.out.println(joe);
     }
 }
